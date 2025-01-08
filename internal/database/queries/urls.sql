d93bc3fcba1b5650a5bedef0b894c9c9aa745181
DarcoProgramador <denangel123@gmail.com>
2025-01-07 22:24:04 -0600

    first commit

---
+++ internal/database/queries/urls.sql
@@ -0,0 +1,40 @@
+-- name: GetURLByShortCode :one
+SELECT 
+    id,
+    url,
+    shortCode,
+    createdAt,
+    updatedAt
+FROM urls
+WHERE shortCode = ?;
+
+-- name: CreateURL :one
+INSERT INTO urls (url, shortCode, createdAt, updatedAt)
+VALUES (?, ?, ?, ?)
+RETURNING id, url, shortCode, createdAt, updatedAt;
+
+-- name: UpdateURLByShortCode :one
+UPDATE urls
+SET url = ?, updatedAt = ?
+WHERE shortCode = ?
+RETURNING id, url, shortCode, createdAt, updatedAt;
+
+-- name: IncrementURLAccessCountByShortCode :exec
+UPDATE urls
+SET accessCount = accessCount + 1
+WHERE shortCode = ?;
+
+-- name: DeleteURLByShortCode :exec
+DELETE FROM urls
+WHERE shortCode = ?;
+
+-- name: GetURLStatsByShortCode :one
+SELECT 
+    id,
+    url,
+    shortCode,
+    createdAt,
+    updatedAt,
+    accessCount
+FROM urls
+WHERE shortCode = ?;
