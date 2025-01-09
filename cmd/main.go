7e760b9f66e36f496bf6d80d8b8a034621e02ae9
David Torres <110353857+DarcoProgramador@users.noreply.github.com>
2025-01-09 22:17:39 +0000

    add database connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/DarcoProgramador/shortener-go-backend/internal/database"
+	db "github.com/DarcoProgramador/shortener-go-backend/internal/database/sqlc"
+)
+
 func main() {
-	// TODO: Implement the main function
+	ctx := context.Background()
+
+	dbSql, err := database.InitDB(ctx)
+	if err != nil {
+		slog.Error("cannot init db", slog.Any("msg", err))
+		os.Exit(1)
+		return
+	}
+
+	queries := db.New(dbSql)
+
+	fmt.Println(queries)
 }
---
+++ internal/database/database.go
@@ -0,0 +1,16 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func InitDB(ctx context.Context) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", "./urls.db")
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
